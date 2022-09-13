bd918045b652f05e7a764f5a0ec589b2a412965f
kuat2111 <32981_kuat>
2022-09-13 23:18:01 +0600

    HW1 is completed

--- hw01_hello_otus/main.go
+++ hw01_hello_otus/main.go
@@ -1,5 +1,19 @@
 package main
 
+import "fmt"
+
+// this is a comment
+
 func main() {
-	// Place your code here.
+	s := "Hello, OTUS!"
+	s = Reverse(s)
+	fmt.Println(s)
+}
+
+func Reverse(s string) string {
+	r := []rune(s)
+	for i, j := 0, len(r)-1; i < len(r)/2; i, j = i+1, j-1 {
+		r[i], r[j] = r[j], r[i]
+	}
+	return string(r)
 }
