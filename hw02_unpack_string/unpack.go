983eaf9e968073846199e0353a3376f418ec98a1
kuat2111 <32981_kuat>
2022-10-03 22:46:35 +0600

    HW2 is completed

--- hw02_unpack_string/unpack.go
+++ hw02_unpack_string/unpack.go
@@ -1,12 +1,91 @@
 package hw02unpackstring
 
 import (
-	"errors"
+	"log"
+	"strconv"
+	"strings"
+	"unicode"
+	"unicode/utf8"
 )
 
-var ErrInvalidString = errors.New("invalid string")
+type Unpacker interface {
+	Unpack() string
+}
 
-func Unpack(_ string) (string, error) {
-	// Place your code here.
-	return "", nil
+type PackedString string
+
+func (s PackedString) Unpack() string {
+	var lastRune, lastLetter rune
+	var result, num strings.Builder
+	var esc bool
+	result.Reset()
+	num.Reset()
+	lastRune = 0
+	lastLetter = 0
+	for i, curRune := range s {
+		// early return
+		if unicode.IsDigit(curRune) && i == 0 {
+			return ""
+		}
+		// if letter writing it to result and optionally unpacking previous sequence
+		if unicode.IsLetter(curRune) {
+			// letter after digit
+			if unicode.IsDigit(lastRune) {
+				numRunes, err := strconv.Atoi(num.String())
+				if err != nil {
+					log.Fatal(err)
+				}
+				for j := 0; j < numRunes-1; j++ {
+					result.WriteRune(lastLetter)
+				}
+				num.Reset()
+			}
+			// any letter
+			result.WriteRune(curRune)
+			lastLetter = curRune
+			lastRune = curRune
+		}
+		// write to digit sequence or flush letters to result
+		if unicode.IsDigit(curRune) {
+			// escape digit
+			if esc {
+				result.WriteRune(curRune)
+				lastLetter = curRune
+				lastRune = curRune
+				esc = false
+			} else {
+				// first digit of new digit sequence
+				if unicode.IsLetter(lastRune) {
+					num.Reset()
+				}
+				num.WriteRune(curRune)
+				lastRune = curRune
+				// last digit in input string
+				if i == utf8.RuneCountInString(string(s))-1 {
+					numRunes, err := strconv.Atoi(num.String())
+					if err != nil {
+						log.Fatal(err)
+					}
+					for j := 0; j < numRunes-1; j++ {
+						result.WriteRune(lastLetter)
+					}
+				}
+			}
+
+		}
+		if curRune == '\\' {
+			if lastRune == '\\' {
+				result.WriteRune(curRune)
+				lastLetter = curRune
+				lastRune = curRune
+				esc = false
+
+			} else {
+				esc = true
+				lastRune = curRune
+			}
+		}
+	}
+
+	return result.String()
 }
--- hw02_unpack_string/unpack_test.go
+++ hw02_unpack_string/unpack_test.go
@@ -1,45 +1,96 @@
 package hw02unpackstring
 
 import (
-	"errors"
 	"testing"
-
-	"github.com/stretchr/testify/require"
 )
 
-func TestUnpack(t *testing.T) {
-	tests := []struct {
-		input    string
-		expected string
-	}{
-		{input: "a4bc2d5e", expected: "aaaabccddddde"},
-		{input: "abccd", expected: "abccd"},
-		{input: "", expected: ""},
-		{input: "aaa0b", expected: "aab"},
-		// uncomment if task with asterisk completed
-		// {input: `qwe\4\5`, expected: `qwe45`},
-		// {input: `qwe\45`, expected: `qwe44444`},
-		// {input: `qwe\\5`, expected: `qwe\\\\\`},
-		// {input: `qwe\\\3`, expected: `qwe\3`},
-	}
+func TestUnpack001(t *testing.T) {
+	var ps PackedString
+	var got, want string
 
-	for _, tc := range tests {
-		tc := tc
-		t.Run(tc.input, func(t *testing.T) {
-			result, err := Unpack(tc.input)
-			require.NoError(t, err)
-			require.Equal(t, tc.expected, result)
-		})
+	ps = "a4bc2d5e"
+	want = "aaaabccddddde"
+	got = ps.Unpack()
+
+	if got != want {
+		t.Errorf("ps.Unpack() == %q, want %q", got, want)
 	}
 }
 
-func TestUnpackInvalidString(t *testing.T) {
-	invalidStrings := []string{"3abc", "45", "aaa10b"}
-	for _, tc := range invalidStrings {
-		tc := tc
-		t.Run(tc, func(t *testing.T) {
-			_, err := Unpack(tc)
-			require.Truef(t, errors.Is(err, ErrInvalidString), "actual error %q", err)
-		})
+func TestUnpack002(t *testing.T) {
+	var ps PackedString
+	var got, want string
+
+	ps = "abcd"
+	want = "abcd"
+	got = ps.Unpack()
+
+	if got != want {
+		t.Errorf("ps.Unpack() == %q, want %q", got, want)
+	}
+}
+
+func TestUnpack003(t *testing.T) {
+	var ps PackedString
+	var got, want string
+
+	ps = "45"
+	want = ""
+	got = ps.Unpack()
+
+	if got != want {
+		t.Errorf("ps.Unpack() == %q, want %q", got, want)
+	}
+}
+
+func TestUnpack004(t *testing.T) {
+	var ps PackedString
+	var got, want string
+
+	ps = `qwe\4\5`
+	want = `qwe45`
+	got = ps.Unpack()
+
+	if got != want {
+		t.Errorf("ps.Unpack() == %q, want %q", got, want)
+	}
+}
+
+func TestUnpack005(t *testing.T) {
+	var ps PackedString
+	var got, want string
+
+	ps = `qwe\45`
+	want = `qwe44444`
+	got = ps.Unpack()
+
+	if got != want {
+		t.Errorf("ps.Unpack() == %q, want %q", got, want)
+	}
+}
+
+func TestUnpack006(t *testing.T) {
+	var ps PackedString
+	var got, want string
+
+	ps = `qwe\\5`
+	want = `qwe\\\\\`
+	got = ps.Unpack()
+
+	if got != want {
+		t.Errorf("ps.Unpack() == %q, want %q", got, want)
+	}
+}
+
+func TestUnpack007(t *testing.T) {
+	var ps PackedString
+	var got, want string
+
+	ps = `\\5`
+	want = `\\\\\`
+	got = ps.Unpack()
+
+	if got != want {
+		t.Errorf("ps.Unpack() == %q, want %q", got, want)
 	}
 }
